0b156dd30a02f62f7e46b88dd7c50c286b3c0025
Srinivas <srinu.java2014@gmail.com>
2025-02-01 13:03:27 -0600

    first base commit

---
+++ src/main/java/com/sb/workflow/sbgit/SbGitWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.sb.workflow.sbgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbGitWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbGitWorkflowApplication.class, args);
+    }
+
+}
